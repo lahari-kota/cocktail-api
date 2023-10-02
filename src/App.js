a778cdb198fea5731449b048df02c587a5238935
lahari kota <108861747+Lahari-max@users.noreply.github.com>
2023-10-02 15:04:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect}from 'react';
+const URL = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
+const Final =() =>{
 
-function App() {
+   const[drinksdata,setDrinksData]= useState([]);
+   const [searchTerm,setsearchTerm] =useState('');
+   const  fetchDrink = async(apiURL)=>{
+    const response = await fetch(apiURL);
+    const {drinks}=await response.json();
+    setDrinksData(drinks);
+   }
+    useEffect(()=>{
+      const correctURL=`${URL}${searchTerm}`
+      fetchDrink(correctURL);
+    },[searchTerm]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='form-container'>
+        <input
+        type="text"
+        className='search'
+        name="search"
+        id="search"
+       placeholder='search something new..'
+       value={searchTerm}
+       onChange={(e) => setsearchTerm(e.target.value )}
+       />
+      </div>
+      <hr />
+      <ul className="cocktail-data">
+        
+          {drinksdata.map((eachDrink) =>{
+            const{idDrink,strDrink,strDrinkThumb}=eachDrink;
+            return (
+              <li key={idDrink}>
+              <div>
+                <img src={strDrinkThumb} alt={strDrink} />
+              </div>
+              <div className="text">
+                <h3>{strDrink}</h3>
+                </div>
+              
+
+            </li>
+            )
+          })
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Final;
